e56f31732efacb7c89ecefd0c004a0924d1ad8ac
Leandro Franciscato <leandro_franciscato@hotmail.com>
2021-11-24 17:02:25 -0300

    removing tests

--- interface.go
+++ interface.go
@@ -3,7 +3,6 @@ package lambda
 import "github.com/elvenworks/lambda-conector/internal/domain"
 
 type ILambda interface {
-	InitLambda(config domain.LambdaConfig) *Lambda
 	GetConfig() *domain.LambdaConfig
 	GetLastLambdaRun() (*domain.LambdaLastRun, error)
 	GetLogsLastErrorRun() (string, error)
--- mock.go
+++ mock.go
@@ -9,29 +9,17 @@ type LambdaMock struct {
 	mock.Mock
 }
 
-func (m LambdaMock) InitLambda(config domain.LambdaConfig) *Lambda {
-	args := m.Called(config)
-
-	return args.Get(0).(*Lambda)
-}
-
 func (m LambdaMock) GetConfig() *domain.LambdaConfig {
-	lam := m.InitLambda(domain.LambdaConfig{})
-	args := lam.GetConfig()
-
-	return args
+	args := m.Called()
+	return args.Get(0).(*domain.LambdaConfig)
 }
 
 func (m LambdaMock) GetLastLambdaRunMock() (*domain.LambdaLastRun, error) {
-	lam := m.InitLambda(domain.LambdaConfig{})
-	args, err := lam.GetLastLambdaRun()
-
-	return args, err
+	args := m.Called()
+	return args.Get(0).(*domain.LambdaLastRun), args.Error(1)
 }
 
 func (m LambdaMock) GetLogsLastErrorRunMock() (string, error) {
-	lam := m.InitLambda(domain.LambdaConfig{})
-	args, err := lam.GetLogsLastErrorRun()
-
-	return args, err
+	args := m.Called()
+	return args.Get(0).(string), args.Error(1)
 }
